40a0f7bfc606f2c4aade8fc0295f14a1f50baaec
PlayerNguyen <daudaua3782@gmail.com>
2021-07-20 13:05:39 +0700

    rebuild a test

--- src/test/java/com/playernguyen/optecoprime/NumberUtilTest.java
+++ src/test/java/com/playernguyen/optecoprime/NumberUtilTest.java
@@ -13,7 +13,6 @@ public class NumberUtilTest {
         // Convert to a number
         NumberFilter filter = new NumberFilter("15.2");
         Assert.assertEquals(15.2, filter.asNumber(), 0.1);
-
     }
 
     @Test
@@ -21,10 +20,13 @@ public class NumberUtilTest {
         // True
         NumberFilter filter = new NumberFilter("150");
         Assert.assertTrue(filter.isNumber());
+    }
+
+    @Test
+    public void shouldNotANumber() {
         // False
         NumberFilter filter1 = new NumberFilter("150x");
         Assert.assertFalse(filter1.isNumber());
-
     }
 
     @Test
@@ -33,6 +35,10 @@ public class NumberUtilTest {
         FlexibleNumber number = new FlexibleNumber(15.978201938);
         Assert.assertEquals("15,98", number.toString());
 
+    }
+
+    @Test
+    public void shouldReturnInitInFlexNumber() {
         // int number
         FlexibleNumber intNumber = new FlexibleNumber(16.0000000);
         Assert.assertEquals("16", intNumber.toString());
